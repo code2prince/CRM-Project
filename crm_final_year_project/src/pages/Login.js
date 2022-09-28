c3214c59450e697522ee3bab7809250467f3197f
github_username <princekumarsharma365@gmail.com>
2022-09-28 16:58:30 +0530

    login page ui

--- crm_final_year_project/src/pages/Login.js
+++ crm_final_year_project/src/pages/Login.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react';
+import '../styles/Styles.css';
 
 function Login() {
+    const [showSignUp, setShowSignUp] = useState(false);
+
+    const toggleSignUp=() =>{
+        setShowSignUp(!showSignUp)
+    }
   return (
-    <div className='container'>Login</div>
+    <div className='container'>
+        
+        <div className='card'>
+        <h3>Login</h3>
+            <div className='row'>
+                <div className='col'>
+                    {
+                        !showSignUp ? 
+                        (<div className='login'>
+                            <form>
+                                <div className='input-group'>
+                                    <input type="text" className='form-control' placeholder='User-Id'/>
+                                </div>
+
+                                <div className='input-group'>
+                                    <input type="password" className='form-control' placeholder='Password'/>
+                                </div>
+
+                                <div className='input-group'>
+                                    <input type="submit" className='form-control btn btn-primary'  value='login'/>
+                                </div>
+                                <div className='text-info'>
+                                    Don't have an account ? signUp
+                                </div>
+                            </form>
+                        </div>
+                        ) :  (
+                        <div className='signUp'>everyone</div>)
+                    }
+                </div>
+            </div>
+
+
+        </div>
+    </div>
   )
 }
 
-export default Login
+export default Login;
